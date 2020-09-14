4a7c0fe9bb0f357631e2b86ef00b0d5d7bed5917
Steve Aldworth <s.aldworth@tvdsb.ca>
2020-09-14 13:31:06 -0400

    Add project files.

---
+++ Part 1 - Basics/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Part_1___Basics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = 99;
+            string name = "Bob";
+
+            Console.WriteLine($"Hi {name}, you are {age} years old.");
+
+            Console.ReadLine();
+        }
+    }
+}
